3af6fde293db2dee0cbc85ba00ac3f9a21a262ca
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-03-12 17:39:30 -0300

    feat: creating header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
-function App() {
-  return <h1>Hello World</h1>;
-}
+import { Header } from './components/Header';
+import './styles/global.css';
 
-export default App;
+export function App() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+      </div>
+    </div>
+  );
+}
