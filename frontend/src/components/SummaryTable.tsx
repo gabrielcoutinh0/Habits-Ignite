744e7dd058d1b8b031a0cca909f05e2a9fc05c08
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-03-14 01:14:03 -0300

    feat: connecting application with backend

--- frontend/src/components/SummaryTable.tsx
+++ frontend/src/components/SummaryTable.tsx
@@ -1,22 +1,40 @@
-import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
-import { HabitDay } from './HabitDay';
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+import { HabitDay } from "./HabitDay";
 
-const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 const summaryDates = generateDatesFromYearBeginning();
 
 const minimumSummaryDatesSize = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
+type Summary = Array<{
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}>;
+
 export function SummaryTable() {
+  const [summary, setSummary] = useState<Summary>([]);
+
+  useEffect(() => {
+    api.get("summary").then((response) => {
+      setSummary(response.data);
+    });
+  }, []);
+
   return (
-    <div className='w-full flex'>
-      <div className='grid grid-rows-7 grid-flow-row gap-3'>
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
         {weekDays.map((weekDay, id) => {
           return (
             <div
               key={`${weekDay}-${id}`}
-              className='text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center'
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
             >
               {weekDay}
             </div>
@@ -24,12 +42,17 @@ export function SummaryTable() {
         })}
       </div>
 
-      <div className='grid grid-rows-7 grid-flow-col gap-3'>
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
+          const dayInSummary = summary.find((day) => {
+            return dayjs(date).isSame(day.date, "day");
+          });
+
           return (
             <HabitDay
-              amount={5}
-              completed={Math.round(Math.random() * 5)}
+              date={date}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
               key={date.toString()}
             />
           );
@@ -40,7 +63,7 @@ export function SummaryTable() {
             return (
               <div
                 key={id}
-                className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed'
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
               />
             );
           })}
